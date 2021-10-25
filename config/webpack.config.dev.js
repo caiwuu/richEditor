7c047ef02c99978ef3ebf20d9be46a2d6ad9196b
caiwuu <2252711582@qq.com>
2021-10-25 17:50:32 +0800

    init

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,31 @@
+'use strict';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const path = require('path');
+
+const devConfig = merge(baseConfig, {
+  mode: 'development',
+  stats: 'none',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
+    },
+    client: {
+      logging: 'none',
+    },
+    hot: true,
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true,
+    }),
+  ],
+});
+module.exports = devConfig;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// 最后一次的range对象
+var lastRange = null;
+// 编辑框dom
+var rangeDivDom = document.querySelector('#range-div');
+// 编辑框点击事件
+rangeDivDom.onclick = function (e) {
+  var selection = document.getSelection();
+  console.log('selection', selection);
+  // 保存最后的range对象
+  lastRange = selection.getRangeAt(0);
+  console.log('selection', lastRange);
+};
+// 编辑框键盘按键松开事件
+rangeDivDom.onkeyup = function (e) {
+  var selection = document.getSelection();
+  console.log('selection', selection);
+  // 保存最后的range对象
+  lastRange = selection.getRangeAt(0);
+  console.log('selection', selection);
+};
+// 插入内容
+window.insertText = function () {
+  var selection = document.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(lastRange);
+  var range = selection.getRangeAt(0);
+  console.log(range);
+  var textNode = range.startContainer;
+  var startOffset = range.startOffset;
+  var insertValue = document.querySelector('#insert-input').value;
+  textNode.insertData(startOffset, insertValue);
+  range.setStart(textNode, startOffset + insertValue.length);
+  selection.addRange(range);
+};
