64deab99fc322b2e6d0d09ee955ecd3be8134801
caiwuu <2252711582@qq.com>
2021-10-28 17:51:23 +0800

    add:model demo

--- src/cursor/index.js
+++ src/cursor/index.js
@@ -1,9 +1,9 @@
-import { styleSet } from '../utils/styleOp'
-import { multiplication } from '../utils/pixelsCalc'
-import Selection from '../selection'
+import { styleSet } from '../utils/styleOp';
+import { multiplication } from '../utils/pixelsCalc';
+import Selection from '../selection';
 export default class Cursor {
-  cursor = null
-  selection = null
+  cursor = null;
+  selection = null;
   meta = {
     x: 0,
     y: 0,
@@ -12,10 +12,10 @@ export default class Cursor {
     selection: null,
     container: null,
     text: null,
-  }
+  };
   constructor() {
-    const cursor = document.createElement('input')
-    cursor.id = 'custom-cursor'
+    const cursor = document.createElement('input');
+    cursor.id = 'custom-cursor';
     const styleObj = {
       top: '-100px',
       left: 0,
@@ -24,11 +24,11 @@ export default class Cursor {
       background: 'transparent',
       border: 'none',
       padding: 0,
-    }
-    styleSet(cursor, styleObj)
-    document.body.appendChild(cursor)
-    this.cursor = cursor
-    this.selection = new Selection()
+    };
+    styleSet(cursor, styleObj);
+    document.body.appendChild(cursor);
+    this.cursor = cursor;
+    this.selection = new Selection();
   }
 
   /**
@@ -36,42 +36,51 @@ export default class Cursor {
    * @memberof Cursor
    */
   setPosition(meta) {
-    this.getMeta()
-    let metaPointer = meta || this.meta
-    const copyStyle = getComputedStyle(metaPointer.container)
-    const lineHeight = multiplication(copyStyle.fontSize, 1.3)
+    this.getMeta();
+    let metaPointer = meta || this.meta;
+    const copyStyle = getComputedStyle(metaPointer.container);
+    const lineHeight = multiplication(copyStyle.fontSize, 1.3);
     const styleObj = {
       top: metaPointer.y + 'px',
       left: metaPointer.x + 'px',
       lineHeight,
       fontSize: copyStyle.fontSize,
       color: copyStyle.color,
-    }
-    styleSet(this.cursor, styleObj)
+    };
+    styleSet(this.cursor, styleObj);
   }
   focus() {
-    this.cursor.focus()
+    this.cursor.focus();
   }
   getMeta() {
-    const range = this.selection.getRange()
-    const text = range.endContainer
+    console.log(this.selection.selection.rangeCount);
+    if (this.selection.selection.rangeCount === 0) {
+      return this.meta;
+    }
+    const range = this.selection.getRange();
+    const text = range.endContainer;
+    console.log(range.endContainer.nodeName);
+    if (text.nodeName !== '#text') {
+      return this.meta;
+    }
     // 相对于 focusTaget 的偏移量
-    const offset = range.endOffset
-    this.meta.range = range
-    this.meta.text = text
-    this.meta.offset = offset
-    this.meta.selection = this.selection
-    this.meta.container = range.endContainer.parentNode
-    // 模拟光标
-    const virtualCursor = document.createElement('span')
-    const endNode = text.splitText(offset)
-    text.parentNode.insertBefore(endNode, text.nextSibling)
-    text.parentNode.insertBefore(virtualCursor, endNode)
-    const { offsetLeft: x, offsetTop: y } = virtualCursor
-    this.meta.x = x
-    this.meta.y = y
-    text.parentNode.removeChild(virtualCursor)
-    text.parentNode.normalize()
-    return this.meta
+    const offset = range.endOffset;
+    this.meta.range = range;
+    this.meta.text = text;
+    this.meta.offset = offset;
+    this.meta.selection = this.selection;
+    this.meta.container = range.endContainer.parentNode;
+    // 模拟光标 有个bug这里
+    const virtualCursor = document.createElement('span');
+    const endNode = text.splitText(offset);
+    text.parentNode.insertBefore(endNode, text.nextSibling);
+    text.parentNode.insertBefore(virtualCursor, endNode);
+    const { offsetLeft: x, offsetTop: y } = virtualCursor;
+    this.meta.x = x;
+    this.meta.y = y;
+    text.parentNode.removeChild(virtualCursor);
+    // 修复行首选区丢失的bug
+    offset && text.parentNode.normalize();
+    return this.meta;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 // 实验性代码，方案验证
-import Cursor from './cursor'
+import Cursor from './cursor';
 // 获取输入区域dom 暂时写死一个
-const input = document.querySelector('#editor-body')
-input.setAttribute('contenteditable', true)
-const cursor = new Cursor()
-input.onclick = function () {
-  cursor.setPosition()
-  cursor.focus()
-}
-document.onkeydown = grabEvent
+const input = document.querySelector('#editor-body');
+input.setAttribute('contenteditable', true);
+const cursor = new Cursor();
+input.onmouseup = function () {
+  cursor.setPosition();
+  cursor.focus();
+};
+document.onkeydown = grabEvent;
 function grabEvent(event) {
-  const keycode = event.which || event.keyCode
-  switch (keycode) {
-    case 37:
-    case 38:
-    case 39:
-    case 40:
+  const key = event.key;
+  switch (key) {
+    case 'ArrowLeft':
+    case 'ArrowUp':
+    case 'ArrowRight':
+    case 'ArrowDown':
+    case 'Backspace':
       if (document.activeElement.id === 'custom-cursor') {
         // 焦点恢复到内容区域以便于使用光标系统
-        const { selection, range, text, offset } = cursor.meta
-        range.setStart(text, offset)
-        range.setEnd(text, offset)
-        selection.removeAllRanges()
-        selection.addRange(range)
+        const { selection, range, text, offset } = cursor.meta;
+        range.setStart(text, offset);
+        range.setEnd(text, offset);
+        selection.removeAllRanges();
+        selection.addRange(range);
       }
   }
 }
 document.onkeyup = function (event) {
-  const keycode = event.which || event.keyCode
-  switch (keycode) {
-    case 37:
-    case 38:
-    case 39:
-    case 40:
+  const key = event.key;
+  switch (key) {
+    case 'ArrowLeft':
+    case 'ArrowUp':
+    case 'ArrowRight':
+    case 'ArrowDown':
+    case 'Backspace':
       // 焦点恢复到 自定义光标上（input）
-      cursor.setPosition()
-      cursor.focus()
+      cursor.setPosition();
+      cursor.focus();
   }
-}
+};
