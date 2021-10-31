f58ecd1fce44a73cde52a4596a15b11db5a3ee28
caiwuu <2252711582@qq.com>
2021-11-01 02:05:22 +0800

    caret fix bug

--- src/cursor/index.js
+++ src/cursor/index.js
@@ -1,14 +1,16 @@
-import { styleSet } from '../utils/styleOp';
-import { multiplication } from '../utils/pixelsCalc';
-import Selection from '../selection';
+import { styleSet } from '../utils/styleOp'
+import { multiplication } from '../utils/pixelsCalc'
+import Selection from '../selection'
+import state from '../state/'
 export default class Cursor {
-  cursor = null;
-  selection = null;
+  input = null
+  caret = null
+  measure = null
+  selection = null
   inputState = {
     value: '',
-    expect: '',
-    done: true,
-  };
+    isComposing: false,
+  }
   meta = {
     x: 0,
     y: 0,
@@ -17,58 +19,50 @@ export default class Cursor {
     selection: null,
     container: null,
     text: null,
-  };
+  }
   constructor() {
-    this.cursor = document.createElement('input');
-    this.initEvent();
-    this.cursor.id = 'custom-cursor';
-    const styleObj = {
-      top: '-100px',
-      left: 0,
-      position: 'absolute',
-      width: '0.5px',
-      background: 'transparent',
-      border: 'none',
-      padding: 0,
-    };
-    styleSet(this.cursor, styleObj);
-    document.body.appendChild(this.cursor);
-    this.selection = new Selection();
+    this.input = document.createElement('input')
+    this.caret = document.createElement('span')
+    this.measure = document.createElement('span')
+    this.initEvent()
+    this.input.id = 'custom-input'
+    this.caret.id = 'custom-caret'
+    this.measure.id = 'custom-measure'
+    document.body.appendChild(this.input)
+    document.body.appendChild(this.caret)
+    document.body.appendChild(this.measure)
+    this.selection = new Selection()
   }
   initEvent() {
-    this.cursor.addEventListener('compositionstart', this.handleEvent.bind(this));
-    // this.cursor.addEventListener('compositionupdate', this.handleEvent.bind(this));
-    // this.cursor.addEventListener('compositionend', this.handleEvent.bind(this));
-    this.cursor.addEventListener('input', this.handleEvent.bind(this));
+    this.input.addEventListener('compositionstart', this.handleEvent.bind(this))
+    this.input.addEventListener('compositionend', this.handleEvent.bind(this))
+    this.input.addEventListener('input', this.handleEvent.bind(this))
   }
   handleEvent(event) {
-    console.log(`--->${event.type}: ${event.data}\n`);
-    if (event.type === this.inputState.expect) {
-      if (event.type === 'input') {
-      }
-    } else {
-    }
-    // 键盘字符输入
+    console.log(`--->${event.type}: ${event.data}--${event.isComposing}--${event.target.value}\n`)
     if (event.type === 'input') {
-      if (this.inputState.done) {
-        this.meta.text.data =
-          this.meta.text.data.slice(0, this.meta.offset) + event.target.value + this.meta.text.data.slice(this.meta.offset);
-        this.setCret(event.target.value.length);
-        this.followSysCret();
-        this.focus();
+      if (!this.inputState.isComposing && event.data) {
+        this.meta.text.data = this.meta.text.data.slice(0, this.meta.offset) + event.data + this.meta.text.data.slice(this.meta.offset)
+        this.setSysCaret(1)
+        this.followSysCaret()
+        this.focus()
       } else {
-        this.inputState.done = false;
-        this.inputState.expect = 'compositionstart';
-        this.inputState.value = event.data;
+        const preValLen = this.inputState.value.length
+        this.inputState.value = event.data || ''
+        this.measure.innerText = this.inputState.value
+        console.log([this.measure.offsetWidth])
+        this.setCustomMeasureSty()
+        this.setCaret(this.meta.x + this.measure.offsetWidth, this.meta.y, this.meta.container)
+        this.meta.text.data = this.meta.text.data.slice(0, this.meta.offset) + this.inputState.value + this.meta.text.data.slice(this.meta.offset + preValLen)
       }
+    } else if (event.type === 'compositionstart') {
+      this.inputState.isComposing = true
+    } else {
+      this.inputState.isComposing = false
+      this.setSysCaret(this.inputState.value.length)
+      this.followSysCaret()
+      this.focus()
     }
-    if (event.type === 'compositionstart') {
-      this.inputState.done = false;
-      this.inputState.expect = 'input';
-    }
-
-    console.log('value', event.target.value);
-    event.target.value = '';
   }
 
   /**
@@ -76,59 +70,94 @@ export default class Cursor {
    * @memberof Cursor
    */
   setPosition(x, y, container) {
-    const copyStyle = getComputedStyle(container);
-    const lineHeight = multiplication(copyStyle.fontSize, 1.3);
+    const copyStyle = getComputedStyle(container)
+    const lineHeight = multiplication(copyStyle.fontSize, 1.3)
     const styleObj = {
       top: y + 'px',
       left: x + 'px',
       lineHeight,
       fontSize: copyStyle.fontSize,
-      color: copyStyle.color,
-    };
-    styleSet(this.cursor, styleObj);
+    }
+    const styleObj2 = {
+      top: y + 'px',
+      left: x + 'px',
+      height: lineHeight,
+      fontSize: copyStyle.fontSize,
+      background: copyStyle.color,
+    }
+    styleSet(this.input, styleObj)
+    styleSet(this.caret, styleObj2)
+    console.log(styleObj2)
   }
+  // 设置自定义光标位置
+  setCaret(x, y, container) {
+    const copyStyle = getComputedStyle(container)
+    const lineHeight = multiplication(copyStyle.fontSize, 1.3)
+    const styleObj = {
+      top: y + 'px',
+      left: x + 'px',
+      height: lineHeight,
+      fontSize: copyStyle.fontSize,
+      background: copyStyle.color,
+    }
+    styleSet(this.caret, styleObj)
+  }
+  //测量中文输入
+  setCustomMeasureSty() {
+    const { container } = this.meta
+    const copyStyle = getComputedStyle(container)
+    const lineHeight = multiplication(copyStyle.fontSize, 1.3)
+    const styleObj = {
+      height: lineHeight,
+      fontSize: copyStyle.fontSize,
+    }
+    styleSet(this.measure, styleObj)
+  }
+  // 聚焦到模拟输入
   focus() {
-    this.cursor.focus();
+    this.input.focus()
   }
-  followSysCret() {
-    this.getMeta();
-    const { x, y, container } = this.meta;
-    this.setPosition(x, y, container);
+  // 自定义光标跟随系统光标
+  followSysCaret() {
+    this.getMeta()
+    const { x, y, container } = this.meta
+    this.setPosition(x, y, container)
   }
-  setCret(relativeOffset) {
-    const { selection, range, text, offset } = this.meta;
-    range.setStart(text, offset + relativeOffset);
-    range.setEnd(text, offset + relativeOffset);
-    selection.removeAllRanges();
-    selection.addRange(range);
+  // 设置系统光标，设置系统光标位置会使模拟输入框失焦
+  setSysCaret(relativeOffset) {
+    const { selection, range, text, offset } = this.meta
+    range.setStart(text, offset + relativeOffset)
+    range.setEnd(text, offset + relativeOffset)
+    selection.removeAllRanges()
+    selection.addRange(range)
   }
   getMeta() {
     if (this.selection.selection.rangeCount === 0) {
-      return this.meta;
+      return this.meta
     }
-    const range = this.selection.getRange();
-    const text = range.endContainer;
+    const range = this.selection.getRange()
+    const text = range.endContainer
     if (text.nodeName !== '#text') {
-      return this.meta;
+      return this.meta
     }
     // 相对于 focusTaget 的偏移量
-    const offset = range.endOffset;
-    this.meta.range = range;
-    this.meta.text = text;
-    this.meta.offset = offset;
-    this.meta.selection = this.selection;
-    this.meta.container = range.endContainer.parentNode;
+    const offset = range.endOffset
+    this.meta.range = range
+    this.meta.text = text
+    this.meta.offset = offset
+    this.meta.selection = this.selection
+    this.meta.container = range.endContainer.parentNode
     // 模拟光标 有个bug这里
-    const virtualCursor = document.createElement('span');
-    const endNode = text.splitText(offset);
-    text.parentNode.insertBefore(endNode, text.nextSibling);
-    text.parentNode.insertBefore(virtualCursor, endNode);
-    const { offsetLeft: x, offsetTop: y } = virtualCursor;
-    this.meta.x = x;
-    this.meta.y = y;
-    text.parentNode.removeChild(virtualCursor);
+    const virtualCursor = document.createElement('span')
+    const endNode = text.splitText(offset)
+    text.parentNode.insertBefore(endNode, text.nextSibling)
+    text.parentNode.insertBefore(virtualCursor, endNode)
+    const { offsetLeft: x, offsetTop: y } = virtualCursor
+    this.meta.x = x
+    this.meta.y = y
+    text.parentNode.removeChild(virtualCursor)
     // 修复行首选区丢失的bug
-    offset && text.parentNode.normalize();
-    return this.meta;
+    offset && text.parentNode.normalize()
+    return this.meta
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
 // 实验性代码，方案验证
-import Cursor from './cursor';
+import Cursor from './cursor'
+import state from './state/'
 // 获取输入区域dom 暂时写死一个
-const input = document.querySelector('#editor-body');
-input.setAttribute('contenteditable', true);
-const cursor = new Cursor();
+const input = document.querySelector('#editor-body')
+input.setAttribute('contenteditable', true)
+const cursor = new Cursor()
 input.onclick = function () {
-  cursor.followSysCret();
-  cursor.focus();
-};
-document.onkeydown = grabEvent;
+  cursor.followSysCaret()
+  cursor.focus()
+}
+document.onkeydown = grabEvent
 function grabEvent(event) {
-  const key = event.key;
+  state.keyboard = event.key
+  console.log(event.key)
+  const key = event.key
   switch (key) {
     case 'ArrowUp':
     case 'ArrowLeft':
     case 'ArrowRight':
     case 'ArrowDown':
-    case 'Backspace':
-      if (document.activeElement.id === 'custom-cursor') {
+      // case 'Backspace':
+      if (document.activeElement.id === 'custom-input') {
         // 焦点恢复到内容区域以便于使用光标系统
-        const { selection, range, text, offset } = cursor.meta;
-        range.setStart(text, offset);
-        range.setEnd(text, offset);
-        selection.removeAllRanges();
-        selection.addRange(range);
+        const { selection, range, text, offset } = cursor.meta
+        range.setStart(text, offset)
+        range.setEnd(text, offset)
+        selection.removeAllRanges()
+        selection.addRange(range)
       }
   }
 }
 document.onkeyup = function (event) {
-  const key = event.key;
+  const key = event.key
   switch (key) {
     case 'ArrowUp':
     case 'ArrowLeft':
     case 'ArrowRight':
     case 'ArrowDown':
-    case 'Backspace':
+      // case 'Backspace':
       // 焦点恢复到 自定义光标上（input）
-      cursor.followSysCret();
-      cursor.focus();
+      cursor.followSysCaret()
+      cursor.focus()
   }
-};
+}
